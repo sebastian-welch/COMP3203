9fa62af2a10a1a3f39e3ddb0d7f0d3b69750e681
Sebastian Welch <sebastianwelch@gmail.com>
2014-10-05 17:03:09 -0400

    Server now accepts ls

--- Assignment1/server.py
+++ Assignment1/server.py
@@ -1,13 +1,19 @@
-import socket
+import socket               
+import subprocess
 
-s = socket.socket()
-host = socket.gethostname()
+s = socket.socket()         
+IP = '192.168.0.107'
 port = 10000
-s.bind((host, port))
+s.bind((IP, port))       
 
 s.listen(5)
+c, addr = s.accept()
 while True:
-    c, addr = s.accept()
-    print 'Connected to: ', addr
-    c.send('Get good nerd')
-    c.close()
+  com = c.recv(5)
+
+  print com;
+
+  if com == 'ls\n':
+    contents = subprocess.Popen(["ls", "-l"], stdout=subprocess.PIPE)
+    out, err = contents.communicate()
+    c.send(out)
