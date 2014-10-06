f074d75e21abf58a48ccde11d64729351b09426b
Shawn Ellis <strategymind@gmail.com>
2014-10-06 08:18:34 -0400

    Added server cleanup on ctrl-c, and handled server returning to listening when client exits. -Z

--- Assignment1/client.py
+++ Assignment1/client.py
@@ -2,16 +2,20 @@ import socket
 import sys
 
 s = socket.socket()
-host = '192.168.0.107'
+host = '10.10.220.108'
 port = 10000
 
 s.connect((host, port))
 while True:
     print "command:"
     command =  sys.stdin.readline()
-    #print "input = ", command
-    if command == "exit\n":
+    command_list = command.split(' ')
+    cmd = command_list[0]
+    s.send(command)
+    if cmd == "exit\n":
         s.close
         break
-    s.send(command)
+    elif cmd == "get":
+        print "get files"
+    
     print s.recv(1024)
